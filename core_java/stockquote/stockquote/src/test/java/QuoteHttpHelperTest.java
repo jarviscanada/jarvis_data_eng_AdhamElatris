c53c872af2a636037f6b5c4dfa0a49db45201d2c
aelatris <adhamelatris@yahoo.com>
2025-02-18 19:16:54 -0500

    Added Junit tests for QuoteHttpHelper and added the dependencies for mokito in pom.xml

---
+++ core_java/stockquote/stockquote/src/test/java/QuoteHttpHelperTest.java
@@ -0,0 +1,35 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import ca.jrvs.apps.stockquote.dao.Quote;
+import ca.jrvs.apps.stockquote.dao.QuoteHttpHelper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class QuoteHttpHelperTest {
+
+  private QuoteHttpHelper quoteHttpHelper;
+
+  @BeforeEach
+  void setUp() {
+    quoteHttpHelper = new QuoteHttpHelper();
+  }
+
+  @Test
+  void testFetchQuoteInfoValidSymbol() {
+    Quote quote = quoteHttpHelper.fetchQuoteInfo("MSFT");
+
+    assertNotNull(quote);
+    assertEquals("MSFT", quote.getTicker());
+    assertTrue(quote.getOpen() >= 408.0);
+    assertTrue(quote.getHigh() >= 410.597);
+    assertTrue(quote.getLow() >= 406.5);
+    assertTrue(quote.getPrice() >= 409.64);
+    assertTrue(quote.getVolume() >= 20544998);
+    assertNotNull(quote.getLatestTradingDay());
+    assertTrue(quote.getPreviousClose() >= 408.43);
+    assertNotNull(quote.getChange());
+    assertNotNull(quote.getChangePercent());
+  }
+}
