432394e5ae117aa9da8c3d2ae175ff12cd8f784e
aelatris <adhamelatris@yahoo.com>
2025-02-23 00:27:18 -0500

    Completed the service layer for Quote

---
+++ core_java/stockquote/stockquote/src/main/java/ca/jrvs/apps/stockquote/service/QuoteService.java
@@ -0,0 +1,42 @@
+package ca.jrvs.apps.stockquote.service;
+
+import ca.jrvs.apps.stockquote.dao.QuoteDao;
+import ca.jrvs.apps.stockquote.dao.QuoteHttpHelper;
+import ca.jrvs.apps.stockquote.model.Quote;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class QuoteService {
+
+  private static final Logger logger = LoggerFactory.getLogger(QuoteService.class);
+
+  private final QuoteDao dao;
+  private final QuoteHttpHelper httpHelper;
+
+  public QuoteService(QuoteDao dao, QuoteHttpHelper httpHelper) {
+    this.dao = dao;
+    this.httpHelper = httpHelper;
+  }
+
+  /**
+   * Fetches latest quote data from endpoint
+   *
+   * @param ticker Stock symbol
+   * @return Latest quote information or empty optional if ticker symbol not found
+   */
+  public Optional<Quote> fetchQuoteDataFromAPI(String ticker) {
+    if (ticker == null || ticker.isBlank()) {
+      logger.error("Invalid symbol");
+    } else {
+      Optional<Quote> quote = Optional.ofNullable(httpHelper.fetchQuoteInfo(ticker));
+      if (quote.isPresent()) {
+        dao.save(quote.get());
+        //logger.info("\nFetched successfully !");
+        return Optional.of(quote.get());
+      }
+    }
+    logger.error("\nError fetching quote, make sure symbol is correct");
+    return Optional.empty();
+  }
+}
